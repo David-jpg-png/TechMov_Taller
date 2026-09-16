6fc886171b80895ad05f50a99760e37bdb8e0bf1
Andres David <acala244@unab.edu.co>
2026-09-16 10:06:28 -0500

    Pantalla usuario añadido

--- app/src/main/java/com/andrescala/tallergithub/MainActivity.kt
+++ app/src/main/java/com/andrescala/tallergithub/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.andrescala.tallergithub.ui.theme.TallerGithubTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,5 +30,48 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
         }
+    }
+}
+
+@Preview (showBackground = true, showSystemUi = true)
+@Composable
+fun Usuario() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.usuario),
+            contentDescription = null,
+            modifier = Modifier.size(200.dp)
+        )
+        Text(
+            text = "Andres David",
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            modifier = Modifier
+                .padding(10.dp)
+            )
+        Text(text = "Estudiante de Ingenieria de sistemas",
+            fontSize = 20.sp,
+            color = Color(39, 127, 213),
+            modifier = Modifier
+                .padding(10.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Edad")
+            Text(text = "21 Años")
+            Text(text = "Correo")
+            Text(text = "acala244@unab.edu.co")
+            Text(text = "Ciudad")
+            Text(text = "Bucaramanga, Colombia")
+        }
+
     }
 }
